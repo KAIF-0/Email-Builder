520869c94e252092b6056ebc81d930a7c9779898
KAIF KHAN <kaif8700979251@gmail.com>
2025-06-25 08:13:35 +0000

    db migrated to neon serverless

--- src/app/api/saveAllTemplates/route.js
+++ src/app/api/saveAllTemplates/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
-    const { templates } = await req.json();
+    const  templates  = await req.json();
     console.log(templates);
 
     await prisma.template.deleteMany();
